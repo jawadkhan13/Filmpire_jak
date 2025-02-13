a46752abc639e3241ec11b95202e43ba70a956e7
Jawad Khane <jawadkhan.cse@gmail.com>
2025-02-13 15:58:30 +0500

    file and folder structure

---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MovieInformation = () => {
+    console.log("Movie information");
+  return (
+    <div>MovieInformation</div>
+  )
+}
+
+export default MovieInformation
