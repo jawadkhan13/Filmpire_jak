f015d01feef2c6a5d4af98f5658a920286c1e16e
Jawad Khane <jawadkhan.cse@gmail.com>
2025-02-15 00:02:05 +0500

    redux setup

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import { Box,CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
-    console.log("Movies ");
+  const { data } = useGetMoviesQuery();
+    console.log( data);
 
   return (
     <div>Movies</div>
   )
 }
 
-export default Movies
+export default Movies;
